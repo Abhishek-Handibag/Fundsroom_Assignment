2167421784eb866754abcca613c4c943aea5d623
Abhishek Handibag <103750036+Abhishek-Handibag@users.noreply.github.com>
2024-02-18 23:14:22 +0530

    Delete stock_tracker directory

--- stock_tracker/stocks/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Share
-
-class ShareForm(forms.ModelForm):
-    class Meta:
-        model = Share
-        fields = ['name', 'purchase_cost', 'current_cost']
